365f1acfbc32be8f8b3acaccef0347546514e1cc
Matthew Obetz <mobetz@gmail.com>
2022-11-11 14:20:36 -0500

    Reorder stylesheets, try setting department from main page.

--- load_projects.js
+++ load_projects.js
@@ -1,14 +1,25 @@
 import STEMExpoProject from "https://mobetz.github.io/stemexpo-gsite-cards/STEMExpoProject.mjs";
 
 
+const department_names = {
+    "Computer Science": ["CS"],
+    "Engineering": ["Engineering"],
+    "Math & Science": ["Biology", "Math"]
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("department_title").innerText = document.department;
+
+
    let projects_promise = STEMExpoProject.getProjects();
 
    let gallery = document.getElementById("gallery");
 
    projects_promise.then((projects) => {
       projects
-          .filter((project) => project.discipline === "Biology" )
+          .filter((project) => department_names[document.department].indexOf(project.discipline) !== -1 )
           .map((project) => project.getCardFromProject())
           .forEach((card) => {
 
